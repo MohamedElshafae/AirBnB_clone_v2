8ad34a6f8ba1407245d9c38911623813a665d0c3
Elshafae <maohmedelshafae888@gmail.com>
2023-09-21 05:20:09 +0300

    fix UnitTest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,9 @@ import json
 import os
 
 
+@unittest.skipIf(
+    os.getenv("HBNB_TYPE_STORAGE") == "db", 
+    "Test is not relevant for BaseModel")
 class test_basemodel(unittest.TestCase):
     """ """
 
