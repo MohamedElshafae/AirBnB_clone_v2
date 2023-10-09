c7324816abaf3c798e657ecb2f8713335344ebe8
Elshafae <maohmedelshafae888@gmail.com>
2023-10-09 20:50:48 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt install -y nginx
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo sh -c 'echo "Hello in this world --__--" > /data/web_static/releases/test/index.html'
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sh -c 'echo "
 server {
