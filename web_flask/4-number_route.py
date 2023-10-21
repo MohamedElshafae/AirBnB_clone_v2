eb5c4bc3c1576ec3db1b4dd7c5a0338c7287064a
Elshafae <maohmedelshafae888@gmail.com>
2023-10-21 21:37:44 +0300

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,5 +29,13 @@ def python(text="is cool"):
     return f"Python {text.replace('_', ' ')}"
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """doc"""
+    if isinstance(n, int):
+        return f'{n} is a number'
+    return None
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
