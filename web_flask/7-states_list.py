6b606f0835020a0f7c0c85cd820c89451bd42696
Elshafae <maohmedelshafae888@gmail.com>
2023-10-22 00:30:54 +0300

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""doc"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_sql_session(e):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """get all states from file storage or DB"""
+    return render_template('7-states_list.html', states=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
