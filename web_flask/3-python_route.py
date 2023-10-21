c254effdc63b9c8e9c4f4de225910738aaf4d40d
Elshafae <maohmedelshafae888@gmail.com>
2023-10-21 21:22:59 +0300

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""doc"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def homepage():
+    """doc"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """doc"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display(text):
+    """doc"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """doc"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
